85184dbee7716200a6a5190b28cfd2d8c34913b8
Sebaspatig <sebaspatig@gmail.com>
2021-11-19 19:32:46 -0500

    Creacion Navbar

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 export default function Layout({children}) {
     return (
         <div className="Layout">
+            <NavBar/>
             {children}
         </div>
     )
